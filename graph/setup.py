e1d3ea69787df591a5b5dde780c9302fb73ac2f6
marinatdd <93993789+marinatdd@users.noreply.github.com>
2022-11-03 19:09:23 +0300

    Create setup.py

---
+++ graph/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='graph',
+      version='0.1',
+      description='graph for python 3',
+      url='http://github.com/marinatdd/diplom/graph',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False,
+)
