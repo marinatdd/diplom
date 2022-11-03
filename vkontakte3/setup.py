209ba9fa63d7261657cb963c4e0903bc65539cf1
marinatdd <93993789+marinatdd@users.noreply.github.com>
2022-11-03 16:53:02 +0300

    Create setup.py

---
+++ vkontakte3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='vkontakte',
+      version='0.1',
+      description='vk.com API for Python 3',
+      url='http://github.com/marinatdd/diplom/vkontakte3/vkontakte',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False,
+)
