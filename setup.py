133a90c9e6c652a5d8d83037f5e301281ccace21
marinatdd <93993789+marinatdd@users.noreply.github.com>
2022-11-03 12:08:06 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,30 +1,10 @@
-from setuptools import setup, find_packages
-import os
+from setuptools import setup
 
-
-setup(
-    url = 'https://gitlab.com/optimization-tasks-in-machine-learning/tz9_merging_library',
-    version = '0.1.0',
-    name = 'tz9_merging_library',
-    description = 'ТЗ9 Объединенная библиотека',
-    author = 'Группа ПМ19-1',
-    author_email = 'leonidalekseevv@mail.ru',
-    license = 'MIT',
-    python_requires = '>=3.7',
-    package_data = {
-        '': ['*.pyc'],
-    },
-    package_dir = {
-        '': os.path.dirname(os.path.realpath(__file__)),
-    },
-    packages = [
-        'tz8_stochastic_optimization',
-        'tz7_cutting_optimisation',
-        'tz6_classification',
-        'tz5_constrained_optimization',
-        'tz4_regression',
-        'tz3_multidimensional_optimization',
-        'tz2_numerical_optimization',
-        'tz1_find_extremums',
-    ],
+setup(name='vkontakte',
+      version='0.1',
+      description='vk.com API for Python 3',
+      url='http://github.com/marinatdd/diplom/vkontakte',
+      license='MIT',
+      packages=['vkontakte'],
+      zip_safe=False,
 )
